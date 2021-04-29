4c0f9ceb7d591b5a86ee0f47f99370ddaf5dc448
kisslas2020 <pinewood82@gmail.com>
2021-04-29 11:01:50 +0200

    done 11:01

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,28 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException("Null argument");
+        }
+
+        Path file = Path.of(output);
+        try (BufferedWriter writer = Files.newBufferedWriter(file)) {
+            for (String name: contacts.keySet()) {
+                String line = String.format("%s: %s", name, contacts.get(name));
+                writer.write(line + "\n");
+            }
+        }
+        catch (IOException ioe) {
+            throw new IllegalStateException("Can not write file", ioe);
+        }
+    }
+}
